8a74819956c57b663c2060aff92f6ca70848739b
Michael Salaverry <michaels@wix.com>
2017-08-14 14:48:57 +0300

    finished ex 1 of https://github.com/wix-private/kickstart-2017/tree/master/react-training/react-templates#exercises

--- src/hello.js
+++ src/hello.js
@@ -2,7 +2,13 @@ define(['react', 'lodash', './hello.rt'], function (React, _, template) {
     'use strict';
 
     return React.createClass({
+        getInitialState: function(){return {name:'bob dole'}},
         displayName: 'Hello',
+        getName: function(ev){
+            let name = ev.target.value;
+            // console.log(name);
+            this.setState({name});
+        },
         render: template
     });
 });
