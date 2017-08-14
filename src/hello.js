68a38d17287e538835e9bb6d86a800d8985e9687
Michael Salaverry <michaels@wix.com>
2017-08-14 14:50:15 +0300

    typo

--- src/hello.js
+++ src/hello.js
@@ -2,7 +2,7 @@ define(['react', 'lodash', './hello.rt'], function (React, _, template) {
     'use strict';
 
     return React.createClass({
-        getInitialState: function(){return {name:'bob dole'}},
+        getInitialState: function(){return {name:'Bob Dole'}},
         displayName: 'Hello',
         getName: function(ev){
             let name = ev.target.value;
