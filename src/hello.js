22786201462685858b1f4333944290b59bd97981
Michael Salaverry <michaels@wix.com>
2017-08-14 16:18:56 +0300

    finished ex 4; lat long state

--- src/hello.js
+++ src/hello.js
@@ -4,11 +4,12 @@ define(['react', 'lodash', './hello.rt'], function (React, _, template) {
     return React.createClass({
         getInitialState: function(){
             let queryParamDefaultName = {};
+            const defLoc = {lat:0,long:0};
             if(window.location.search !== ''){
                 // console.log(window.location.search);
-                queryParamDefaultName = {name: window.location.search.match(/name=(.*)/).pop()};
+                queryParamDefaultName = {name: window.location.search.match(/name=(.*)/).pop(),location:defLoc};
             } else {
-                queryParamDefaultName = {name: 'Bobby Dole'};
+                queryParamDefaultName = {name: 'Bobby Dole',location:defLoc};
             }
             return queryParamDefaultName;
         },
@@ -18,8 +19,13 @@ define(['react', 'lodash', './hello.rt'], function (React, _, template) {
             // console.log(name);
             this.setState({name});
         },
+        changeLocation: function(ev,latOrLong){
+            // console.log(ev.target.value,latOrLong)
+            let newCoordinate = parseFloat(ev.target.value);
+            let otherCoord = latOrLong==='lat'?'long':'lat';
+            this.setState({name: this.state.name,location:{[latOrLong]: newCoordinate,[otherCoord]:this.state.location[otherCoord]}});
+            // console.log(this.state.location.lat, this.state.location.long);
+        },
         render: template
     });
 });
-
-// Enhance the previous project so that an initial value for the name field can be provided via the URL.
